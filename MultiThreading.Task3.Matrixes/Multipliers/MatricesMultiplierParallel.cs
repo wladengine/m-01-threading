d6fc3598a780bd10ae2b4d805d266a5b905f3a9c
Vladislav Chikhira <Vladislav_Chikhira@epam.com>
2023-05-01 18:28:35 +0300

    Task #3: cleanup old code

--- MultiThreading.Task3.Matrixes/Multipliers/MatricesMultiplierParallel.cs
+++ MultiThreading.Task3.Matrixes/Multipliers/MatricesMultiplierParallel.cs
@@ -1,31 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MultiThreading.Task3.MatrixMultiplier.Matrices;
 
 namespace MultiThreading.Task3.MatrixMultiplier.Multipliers;
 
 public class MatricesMultiplierParallel : IMatricesMultiplier
 {
-    private readonly object syncLock = new object();
     public IMatrix Multiply(IMatrix m1, IMatrix m2)
     {
-        // todo: feel free to add your code here
         var resultMatrix = new Matrix(m1.RowCount, m2.ColCount);
-
-        //Parallel.For(
-        //    fromInclusive: 0, 
-        //    toExclusive: m1.RowCount,
-        //    body: idx =>
-        //    {
-        //        long[] vector = CalculateVector(m1, m2, idx);
-        //        lock (syncLock)
-        //        {
-        //            for (var i = 0; i < vector.Length; i++)
-        //            {
-        //                resultMatrix.SetElement(idx, i, vector[i]);
-        //            }
-        //        }
-        //    });
         
         Parallel.For(
             fromInclusive: 0, 
@@ -36,23 +18,6 @@ public class MatricesMultiplierParallel : IMatricesMultiplier
             });
 
         return resultMatrix;
-    }
-
-    private static long[] CalculateVector(IMatrix m1, IMatrix m2, long index)
-    {
-        var resultVector = new long[m2.ColCount];
-        for (byte j = 0; j < m2.ColCount; j++)
-        {
-            long sum = 0;
-            for (byte k = 0; k < m1.ColCount; k++)
-            {
-                sum += m1.GetElement(index, k) * m2.GetElement(k, j);
-            }
-
-            resultVector[j] = sum;
-        }
-
-        return resultVector;
     }
     
     private static void CalculateVectorWithDirectWrite(IMatrix m1, IMatrix m2, long index, Matrix resultMatrix)
