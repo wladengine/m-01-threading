2c97d97ba66f5d232b9718a1b9a10ad3f0515eff
Vladislav Chikhira <Vladislav_Chikhira@epam.com>
2023-05-14 14:49:52 +0300

    Added inner sync semaphore

--- MultiThreading.Task4.Threads.Join/Program.cs
+++ MultiThreading.Task4.Threads.Join/Program.cs
@@ -16,7 +16,8 @@ namespace MultiThreading.Task4.Threads.Join;
 
 class Program
 {
-    private static readonly Semaphore _semaphore = new(initialCount: 0, maximumCount: MaxThreads);
+    private static readonly Semaphore _mainSemaphore = new(initialCount: 0, maximumCount: 1);
+    private static readonly Semaphore _innerSemaphore = new(initialCount: 1, maximumCount: 1);
     private const int MaxThreads = 10;
     static void Main(string[] args)
     {
@@ -63,20 +64,23 @@ class Program
         Console.WriteLine("Doing work with ThreadPool...");
         Console.WriteLine($"Current thread is {Environment.CurrentManagedThreadId:D2}");
         ThreadPool.QueueUserWorkItem(DoWorkWithPool, MaxThreads);
-        _semaphore.WaitOne();
+        _mainSemaphore.WaitOne();
         Console.WriteLine("Done.");
         Console.WriteLine();
     }
     private static void DoWorkWithPool(object state)
     {
+        _innerSemaphore.WaitOne();
         Console.WriteLine($"Current state is {state:D2}; Current thread number is {Environment.CurrentManagedThreadId:D2}");
         state = (int)state - 1;
         if ((int)state <= 0)
         {
-            _semaphore.Release();
+            _innerSemaphore.Release();
+            _mainSemaphore.Release();
             return;
         }
-            
+
         ThreadPool.QueueUserWorkItem(DoWorkWithPool, state);
+        _innerSemaphore.Release();
     }
 }
